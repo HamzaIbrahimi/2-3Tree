68e4821ed88250edd3703c44f31da2ea1d414eb4
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2025-11-02 19:26:29 +0100

    Ensure that the main methods work correctly for the root

--- src/lib/classes/test/twoThreeTree.spec.ts
+++ src/lib/classes/test/twoThreeTree.spec.ts
@@ -1,15 +1,41 @@
 import { describe, it, expect, beforeEach } from 'vitest';
-import TreeNode from '../node';
 import TwoThreeTree from '../twoThreeTree';
 
-let twoThreeTree: TwoThreeTree;
+let tree: TwoThreeTree;
 
 beforeEach(() => {
-	twoThreeTree = new TwoThreeTree();
+	tree = new TwoThreeTree();
 });
 
 describe('Has root on initialization', () => {
 	it('has a root', () => {
-		expect(twoThreeTree.root).toBeNull();
+		expect(tree.root).toBeNull();
+	});
+});
+
+describe('A key can be inserted into the tree', () => {
+	it('inserts a key into the tree', () => {
+		tree.insert('A');
+		expect(tree.root?.keys[0]).toBe('A');
+	});
+});
+
+describe('If 3 keys are inserted into the root, it is split into 3 nodes', () => {
+	it('inserts 3 keys into the root', () => {
+		tree.insert('A');
+		tree.insert('B');
+		tree.insert('C');
+		expect(tree.root?.left?.keys[0]).toBe('A');
+		expect(tree.root?.keys[0]).toBe('B');
+		expect(tree.root?.right?.keys[0]).toBe('C');
+	});
+});
+
+describe('Should find any key on the root', () => {
+	it('Adds 3 keys and finds them', () => {
+		tree.insert('A');
+		tree.insert('B');
+		tree.insert('C');
+		expect(tree.find('A')).toEqual(tree.root?.left);
 	});
 });
--- src/lib/classes/twoThreeTree.ts
+++ src/lib/classes/twoThreeTree.ts
@@ -2,13 +2,63 @@ import TreeNode from './node';
 
 export default class TwoThreeTree {
 	root: TreeNode | null;
+	#size: number;
 	constructor() {
 		this.root = null;
+		this.#size = 0;
 	}
 
-	insert(key: string) {}
+	insert(key: string) {
+		const node = new TreeNode();
+		if (this.root === null) {
+			node.insertKey(key);
+			this.root = node;
+			this.#size++;
+			return;
+		}
 
-	find() {}
+		if (!this.root.isFull()) {
+			this.root.insertKey(key);
+			this.#size++;
+			if (this.root.isFull()) {
+				this.root = this.#splitRoot(this.root);
+			}
+			return;
+		}
+	}
+
+	find(key: string) {
+		let node = this.root;
+		while (node !== null) {
+			if (node.keys.includes(key)) {
+				return node;
+			} else if (key < node.keys[0]) {
+				node = node.left;
+			} else if (node.isThreeNode() && key < node.keys[1]) {
+				node = node.middle;
+			} else {
+				node = node.right;
+			}
+		}
+	}
 
 	delete() {}
+
+	get size() {
+		return this.#size;
+	}
+
+	//If the root has 3 keys split into a 2node
+	#splitRoot(node: TreeNode) {
+		const [leftKey, middleKey, rightKey] = node.keys;
+		const newRoot = new TreeNode();
+		newRoot.insertKey(middleKey);
+		newRoot.left = new TreeNode();
+		newRoot.left.insertKey(leftKey);
+		newRoot.right = new TreeNode();
+		newRoot.right.insertKey(rightKey);
+		newRoot.right.parent = newRoot;
+		newRoot.left.parent = newRoot;
+		return newRoot;
+	}
 }
