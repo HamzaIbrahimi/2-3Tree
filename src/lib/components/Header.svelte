3c358f02312eb961db215c3380bc1a807c50db95
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2025-11-05 09:43:29 +0100

    Remove the li html element and use a only

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,21 +1,24 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	let navigations = ['Learn', 'About'];
 </script>
 
-<nav class="border-bottom border-black-200 flex justify-between border-b px-5 py-1">
+<nav class="flex justify-between px-5 py-1">
 	<header>
-		<a href="/" class="flex items-center justify-center gap-2">
-			<h1 class="text-xl">2-3 Tree Visualizer</h1>
+		<a href="/" class="flex items-center justify-center gap-1">
+			<h1 class="text-m">2-3 Tree Visualizer</h1>
 			<figure>
 				<img src={favicon} alt="A tree that depicts the two three tree data structure" />
 			</figure>
 		</a>
 	</header>
 
-	<ul
-		class="flex items-center justify-center gap-3 [&>li]:cursor-pointer [&>li]:rounded-md [&>li]:border [&>li]:px-5 [&>li]:transition-shadow [&>li]:hover:shadow-md"
-	>
-		<li><a href="/learn">Learn</a></li>
-		<li><a href="/about">About</a></li>
-	</ul>
+	<div class="flex items-center justify-center gap-3">
+		{#each navigations as nav}
+			<a
+				class="ccursor-pointer rounded-full bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+				href={nav.toLowerCase()}>{nav}</a
+			>
+		{/each}
+	</div>
 </nav>
