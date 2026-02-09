08b90ef70b0c902563058ea3ec628d583cf7a361
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2026-02-09 17:01:19 +0100

    Add a history component to show the user the recent interactions

---
+++ src/lib/components/History.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { SvelteDate } from 'svelte/reactivity';
+
+	type Ops = {
+		operation: string;
+		letter: string;
+		date: Date;
+	};
+
+	interface Props {
+		opsHistory: Ops[];
+	}
+	const pad = (n: number) => (n < 10 ? '0' + n : n);
+	let { opsHistory }: Props = $props();
+</script>
+
+<div class="container">
+	<div class="title">Operation History</div>
+	<div class="ops-container">
+		{#each opsHistory as ops}
+			<div class="ops">
+				<div>
+					<div class="operation">{ops.operation}</div>
+					<div>{ops.letter}</div>
+				</div>
+				<div class="timestamp">
+					{ops.date.getHours()}:{pad(ops.date.getMinutes())}:{pad(ops.date.getSeconds())}
+				</div>
+			</div>
+		{/each}
+	</div>
+</div>
+
+<style>
+	.container {
+		padding: 1rem 1rem;
+	}
+</style>
