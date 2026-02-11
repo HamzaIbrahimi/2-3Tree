0030438116ca284b09ceb8c50c3ff1536c84f874
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2026-02-11 16:18:02 +0100

    Add a button to reset the tree and display the size of the keys

--- src/lib/components/TreeVisualizer.svelte
+++ src/lib/components/TreeVisualizer.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from 'svelte';
 	import TwoThreeTree from '$lib/classes/twoThreeTree';
 	import TreeVisualizer from '$lib/visualization/TreeVisualizer';
+	import reset from '$lib/assets/debug-restart.svg';
 
 	// Props for parent component to control the tree
 	interface Props {
@@ -14,6 +15,7 @@
 
 	let container = $state<HTMLDivElement>();
 	let tree = $state(new TwoThreeTree());
+	let size = $state(0);
 	let visualizer = $state<TreeVisualizer>();
 
 	onMount(() => {
@@ -29,6 +31,7 @@
 
 		tree.insert(value);
 		await visualizer.animateInsert(value);
+		size = tree.size();
 		if (onInsert) await onInsert(value);
 	}
 
@@ -56,6 +59,7 @@
 		tree = new TwoThreeTree();
 		if (visualizer) {
 			visualizer.setTree(tree);
+			size = 0;
 			visualizer.draw();
 		}
 	}
@@ -69,11 +73,54 @@
 	}
 </script>
 
-<div class="tree-canvas" bind:this={container}></div>
+<div class="tree-canvas" bind:this={container}>
+	<button onclick={() => clear()} class="clear">
+		<img src={reset} alt="reset the" />
+	</button>
+	<span class="size">Tree Size: {size}</span>
+</div>
 
 <style>
 	.tree-canvas {
+		position: relative;
 		width: 100%;
 		height: 100%;
+		border: 1px solid var(--border-medium);
+	}
+
+	button {
+		border: 1px solid var(--border-medium);
+		background-color: var(--bg-slate-100);
+		padding: 0.3rem 2rem;
+		border-radius: 2vw;
+		font-size: 0.9rem;
+		cursor: pointer;
+		display: flex;
+		flex-direction: row-reverse;
+		gap: 1rem;
+	}
+
+	img {
+		width: 20px;
+		height: 20px;
+		transition: 500ms ease-out;
+	}
+
+	button:hover img {
+		transform: rotate(360deg);
+		transition: 500ms ease-in;
+	}
+
+	.size,
+	.clear {
+		position: absolute;
+		top: 0;
+		margin: 1rem;
+	}
+	.size {
+		right: 0;
+	}
+	.clear {
+		left: 0;
 	}
 </style>
