6a0536c6c9e5c4470b3abc4e4f6af22422860345
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2026-02-16 10:56:46 +0100

    Add helpful meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.css';
-	import '../reset.css'
+	import '../reset.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import Header from '$lib/components/Header.svelte';
 
